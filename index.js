37dbfec2b6ae73031961674dc9dacda13313abca
David Felix <david.felix02@hotmail.com>
2020-09-24 02:56:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function saturdayFun(activity = "roller-skate") {
 }
 
 
-function mondayWork(activity = "roller-skate") {
-  return `This Saturday, I want to ${activity}!`
+function mondayWork(activity = "go to the office") {
+  return `This monday, I want to ${activity}!`
 }
 
 mondayWork
