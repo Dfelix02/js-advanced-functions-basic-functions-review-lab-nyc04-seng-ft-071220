196e55f28a3183350d6309d1fa51728348940bd3
David Felix <david.felix02@hotmail.com>
2020-09-24 02:58:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ function mondayWork(activity = "go to the office") {
   return `This Monday, I will ${activity}.`
 }
 
-wrapAdjective
+function wrapAdjective(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`
+}
